ff7317b40722ffa52864aec32d1606b5b16cfb89
hujl <hujl0223@163.com>
2021-04-23 15:24:18 +0800

    获取属性

---
+++ get_attr.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+
+def get_attr():
+    linedic={}
+    with open('2','r',encoding='utf-8') as ra:
+        for line in ra:
+            line_list=line.strip().split('=')
+            linedic.setdefault(line_list[0],[]).append(line_list[1])
+    # print(linedic)
+    with open('b','r',encoding='utf-8') as rb:
+        for line in rb:
+            line_b=line.strip().split('=')
+            if line_b[0].strip()!='{' and line_b[0].strip()!='}':
+                if line_b[0][:2].strip()!='17':
+                    expl=linedic.get(line_b[0],'/**/')
+                    event_attr=line.strip()+''.join(expl).strip()
+                else:
+                    expl='/*项目化的属性*/'
+                    event_attr = line.strip()+''.join(expl)
+                event_attr=event_attr.replace(',','')
+                print(event_attr.strip())
+
+
+if __name__=='__main__':
+    get_attr()
